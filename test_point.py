74cb5dd64a4985ba3a137f8624369fe9c3a5abf7
aleksandr.karev <aleksandr.karev@danfoss.com>
2021-09-20 15:47:40 +0300

    add eq to Poit

--- test_point.py
+++ test_point.py
@@ -10,3 +10,15 @@ def test_point_code():
     p2 = Point(20, False)
     assert p1.code(1.0) == 'Y10.000\nM3\n'
     assert p2.code(1.0) == 'Y20.000\nM5\n'
+
+def test_point_equal():
+    p1 = Point(30, True)
+    p2 = Point(30, True)
+    assert p1 == p2
+
+def test_point_not_equal():
+    p1 = Point(40, True)
+    p2 = Point(50, True)
+    assert p1 != p2
+    p3 = Point(40, False)
+    assert p1 != p3

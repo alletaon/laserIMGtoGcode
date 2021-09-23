4adb3994612761c4ab7c18deb012c884adbdd840
aleksandr.karev <aleksandr.karev@danfoss.com>
2021-09-23 12:02:32 +0300

    add generation progress

--- window.py
+++ window.py
@@ -1,7 +1,8 @@
 from PIL import ImageTk, Image
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 from laser import Layer
+from threading import Thread
 
 
 MAX_IMG_WIDTH = 800
@@ -12,23 +13,32 @@ class App(tk.Frame):
         tk.Frame.__init__(self, master)
         self.master.title('BMPtoGcode')
         self.master.iconbitmap('.\\laser.ico')
-        frame = tk.Frame(self,)
-        frame.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-        tk.Button(frame, text="Открыть", command=self.open).pack(side=tk.LEFT, padx=5)
-        tk.Label(frame, text='Шаг: ').pack(side=tk.LEFT, padx=5)
+        toolbar = tk.Frame(self,)
+        toolbar.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+        tk.Button(toolbar, text="Открыть", command=self.open).pack(side=tk.LEFT, padx=5)
+        tk.Label(toolbar, text='Шаг: ').pack(side=tk.LEFT, padx=5)
         self.step = tk.DoubleVar(value=0.125)
-        tk.Entry(frame, textvariable=self.step).pack(side=tk.LEFT, padx=5)
-        tk.Label(frame, text='Скорость: ').pack(side=tk.LEFT, padx=5)
+        tk.Entry(toolbar, textvariable=self.step).pack(side=tk.LEFT, padx=5)
+        tk.Label(toolbar, text='Скорость: ').pack(side=tk.LEFT, padx=5)
         self.speed = tk.IntVar(value=100)
-        tk.Entry(frame, textvariable=self.speed).pack(side=tk.LEFT, padx=5)
-        tk.Button(frame, text="Сгенерировать", command=self.generate).pack(side=tk.LEFT, padx=5)
-        frame.pack(side=tk.TOP, pady=10, fill=tk.BOTH)
+        tk.Entry(toolbar, textvariable=self.speed).pack(side=tk.LEFT, padx=5)
+        tk.Button(toolbar, text="Сгенерировать", command=self.generate).pack(side=tk.LEFT, padx=5)
+        toolbar.pack(side=tk.TOP, pady=10, fill=tk.BOTH)
 
         self.file_path = tk.StringVar()
         tk.Label(self, textvariable=self.file_path).pack(side=tk.TOP, fill=tk.BOTH)
 
         self.la = tk.Label(self)
         self.la.pack(padx=5, pady=5)
+
+        statusbar = tk.Frame(self,)
+        tk.Label(statusbar, text='Статус генерации: ').pack(side=tk.LEFT, padx=5)
+        self.progress = ttk.Progressbar(statusbar, orient='horizontal', mode='determinate', length=150, value=0)
+        self.progress.pack(side=tk.LEFT, padx=5)
+        self.time = tk.StringVar()
+        tk.Entry(statusbar, textvariable=self.time, state=tk.DISABLED).pack(side=tk.RIGHT, padx=5)
+        tk.Label(statusbar, text='Время выполнения: ').pack(side=tk.RIGHT, padx=5)
+        statusbar.pack(side=tk.BOTTOM, pady=10, fill=tk.BOTH)
 
         self.pack()
 
@@ -52,13 +62,17 @@ class App(tk.Frame):
     def generate(self):
         filename = filedialog.asksaveasfilename()
         if filename != "":
-            self.set_layer(filename)
+            gen_thread = Thread(target=self.set_layer, args=(filename,))
+            gen_thread.start()
 
     def set_layer(self, filename):
+        self.progress['value'] = 0
         self.layer = Layer(0, list(self.im.getdata()), self.im.width)
+        self.progress['value'] = 50
         out = open(filename, 'w')
         out.writelines(self.layer.code(self.step.get(), self.speed.get()))
         out.close()
+        self.progress['value'] = 100
 
 
 if __name__ == "__main__":
